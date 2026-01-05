a4c53103e94c2b41440db4c782e3ee3bfd3a1285
meghabiradar10 <meghabiradar1011@gmail.com>
2026-01-06 01:52:20 +0530

    Added largest numbers program

---
+++ src/arrays/MAXNumber.java
@@ -0,0 +1,18 @@
+package arrays;
+import java.util.*;
+public class MAXNumber {
+    public static int getLargest(int numbers[]) {
+        int largest = Integer.MIN_VALUE;
+        for(int i = 0; i < numbers.length; i++) {
+            if(largest < numbers[i]) {
+                largest = numbers[i];
+            }
+        }
+       return largest;
+    }
+    public static void main(String args[]) {
+        int numbers[] = {1, 2, 6, 3, 5};
+
+        System.out.println(" value is :" +getLargest(numbers));
+    }
+}
